9f09feb5cf0a0e048b9c3ef510e2c8d633447bbd
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-06-09 11:18:45 -0300

    aula 08 | Product

--- projeto-final/db/db_store.sql
+++ projeto-final/db/db_store.sql
@@ -4,11 +4,23 @@ CREATE DATABASE db_store;
 -- selecionar o banco --
 USE db_store;
 
--- criar tabela
+-- criar tabela category
 CREATE TABLE tb_category (
     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     description VARCHAR (100) NOT NULL
+);
+
+-- criar tabela product
+CREATE TABLE tb_product (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    photo VARCHAR(255) NOT NULL,
+    valor FLOAT(5,2) NOT NULL,
+    categoria_id int(11) NOT NULL,
+    quantity INT(5) NOT NULL,
+    created_at DATETIME NOT NULL
 );
 
 --Inserir dados na tabela tb_category
