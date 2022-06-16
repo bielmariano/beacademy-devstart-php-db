49ec7daf86bd62a8a406a1b650bb78083b14a907
Gabriel Mariano <gabrielmbdev@gmail.com>
2022-06-16 18:31:55 -0300

    Aula 20 | Editando Produto

--- projeto-final/src/View/product/edit.php
+++ projeto-final/src/View/product/edit.php
@@ -1,1 +1,27 @@
 <h1>Editar Produtos</h1>
+
+<?php 
+extract($data)
+?>
+
+<form action="" method="post">
+    
+
+    <label for="name">Nome</label>
+    <input value="<?php echo $product['name'] ?>" id="name" name="name" type="text" class="form-control mb-3">
+
+    <label for="description" method="post">Descrição</label>
+    <textarea  name="description" id="description" class="form-control mb-3"><?php echo $product['description']; ?></textarea>
+
+    <label for="value">Preço</label>
+    <input value="<?php echo $product['value'] ?>" id="value" name="value" type="text" class="form-control mb-3">
+
+    <label for="quantity">Quantidade</label>
+    <input value="<?php echo $product['quantity'] ?>" id="quantity" name="quantity" type="text" class="form-control mb-3">
+
+    <label for="photo">Foto</label>
+    <input value="<?php echo $product['photo'] ?>" id="photo" name="photo" type="text" class="form-control mb-3">
+
+    <button class="btn btn-primary">Atualizar</button>
+
+</form>
